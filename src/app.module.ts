4ac4a614e0b57c15f3f2dddd91a1193e5085a043
Guzman <david.a.guzman26@gmail.com>
2024-05-08 10:37:21 -0500

    Creacion de las tablas de la base de datos

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,15 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './users/users.module';
+import { User } from './users/entities/user.entity';
+import { TaskCategoriesModule } from './task_categories/task_categories.module';
+import { TaskCategory } from './task_categories/entities/task_category.entity';
+import { TasksStatusModule } from './tasks_status/tasks_status.module';
+import { TasksStatus } from './tasks_status/entities/tasks_status.entity';
+import { TasksModule } from './tasks/tasks.module';
+import { Task } from './tasks/entities/task.entity';
+
 
 @Module({
   imports: [TypeOrmModule.forRoot({
@@ -11,10 +20,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
     username: 'root',
     password: '',
     database: 'apicul',
-    entities: [],
+    entities: [User,TaskCategory,TasksStatus,Task],
     synchronize: true,
-  })],
+  }), UsersModule, TaskCategoriesModule, TasksStatusModule, TasksModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
---
+++ src/tasks/entities/task.entity.ts
@@ -0,0 +1,22 @@
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Task {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    task_category_id: number;
+
+    @Column()
+    task_status_id: number;
+
+    @Column()
+    user_id: number;
+
+    @Column()
+    description: string;
+
+    @Column()
+    color: string;
+}
